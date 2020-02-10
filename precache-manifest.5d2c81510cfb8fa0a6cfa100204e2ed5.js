667e2f349fea16adac65c1686082c41813fb8ff8
Pål-Christian By <Pal.christian.by@gmail.com>
2020-02-10 14:51:11 +0100

    Updates

---
+++ precache-manifest.5d2c81510cfb8fa0a6cfa100204e2ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6293537551b7186825b5ffdfc95a67c1",
+    "url": "/board-game-shop/index.html"
+  },
+  {
+    "revision": "c20f98c97e2384b21eb5",
+    "url": "/board-game-shop/static/css/main.5bacf575.chunk.css"
+  },
+  {
+    "revision": "89cbe1b250f5a149097e",
+    "url": "/board-game-shop/static/js/2.73ba89db.chunk.js"
+  },
+  {
+    "revision": "ccb800b968167620613463d593f41ad8",
+    "url": "/board-game-shop/static/js/2.73ba89db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c20f98c97e2384b21eb5",
+    "url": "/board-game-shop/static/js/main.91f5b575.chunk.js"
+  },
+  {
+    "revision": "3d59a45696bb91248b2c",
+    "url": "/board-game-shop/static/js/runtime-main.e4e5ca98.js"
+  }
+]);
