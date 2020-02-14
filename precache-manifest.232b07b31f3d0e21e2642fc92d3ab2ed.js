a648da371141defff49e901d3ec6ff49af7af4e9
Pål-Christian By <Pal.christian.by@gmail.com>
2020-02-14 10:31:45 +0100

    Updates

--- precache-manifest.232b07b31f3d0e21e2642fc92d3ab2ed.js
+++ precache-manifest.232b07b31f3d0e21e2642fc92d3ab2ed.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "107b1906ae6288a772bd3f1178a11f3b",
+    "revision": "e214256566fbeb8724c4e952dc22d7cc",
     "url": "/board-game-shop/index.html"
   },
   {
-    "revision": "af376da2191f35543e29",
-    "url": "/board-game-shop/static/css/main.eed2faf9.chunk.css"
+    "revision": "8fa620b8eb6f93856f91",
+    "url": "/board-game-shop/static/css/main.17510e4a.chunk.css"
   },
   {
     "revision": "89cbe1b250f5a149097e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/board-game-shop/static/js/2.73ba89db.chunk.js.LICENSE"
   },
   {
-    "revision": "af376da2191f35543e29",
-    "url": "/board-game-shop/static/js/main.b00a01b1.chunk.js"
+    "revision": "8fa620b8eb6f93856f91",
+    "url": "/board-game-shop/static/js/main.4ce7e7ee.chunk.js"
   },
   {
     "revision": "3d59a45696bb91248b2c",
